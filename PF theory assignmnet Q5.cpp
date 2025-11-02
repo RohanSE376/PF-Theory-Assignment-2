a0e59cce91aa9945a757f7e450da68ef4dee9b03
Muhammad Rohan Ur Rehman <K253055@nu.edu.pk>
2025-11-02 13:07:14 +0500

    Update PF theory assignmnet Q5.cpp

--- PF theory assignmnet Q5.cpp
+++ PF theory assignmnet Q5.cpp
@@ -1,87 +1,104 @@
 #include <stdio.h>
-#include <string.h>
 
+void readString(char buffer[], int size) {
+    int i = 0;
+    char c;
+    while (i < size - 1) {
+        c = getchar();
+        if (c == '\n') {
+            break;
+        }
+        buffer[i] = c;
+        i++;
+    }
+    buffer[i] = '\0';
+}
 
-void encodeMessage(char message[]);
-void decodeMessage(char message[]);
-void toggleBits(char *ch);
+int getStringLength(char s[]) {
+    int i = 0;
+    while (s[i] != '\0') {
+        i++;
+    }
+    return i;
+}
+
+void reverseString(char s[]) {
+    int start = 0;
+    int end = getStringLength(s) - 1;
+    char temp;
+
+    while (start < end) {
+        temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void toggleMessageBits(char s[]) {
+    int i = 0;
+    int mask = (1 << 1) | (1 << 4); 
+
+    while (s[i] != '\0') {
+        s[i] = s[i] ^ mask;
+        i++;
+    }
+}
+
+void encodeMessage(char message[]) {
+    reverseString(message);
+    toggleMessageBits(message);
+}
+
+void decodeMessage(char message[]) {
+    toggleMessageBits(message);
+    reverseString(message);
+}
 
 int main() {
-    char message[200];
+    char message[256];
     int choice;
 
-    while (1) {
-        
+    do {
+        printf("\n--- TCS Secure Message System ---\n");
         printf("1. Encode Message\n");
         printf("2. Decode Message\n");
         printf("3. Exit\n");
+        printf("---------------------------------\n");
         printf("Enter your choice: ");
-        scanf("%d", &choice);
-        getchar(); 
+
+        if (scanf("%d", &choice) != 1) {
+            printf("Invalid input.\n");
+            while(getchar() != '\n'); 
+            continue;
+        }
+        
+        while(getchar() != '\n');
 
         switch (choice) {
             case 1:
-                printf("\nEnter message to encode: ");
-                gets(message);
+                printf("Enter message to encode: ");
+                readString(message, 256);
                 encodeMessage(message);
-                printf("\nEncoded Message: %s\n", message);
+                printf("Encoded message: %s\n", message);
                 break;
-
             case 2:
-                printf("\nEnter message to decode: ");
-                gets(message);
+                printf("Enter message to decode: ");
+                readString(message, 256);
                 decodeMessage(message);
-                printf("\nDecoded Message: %s\n", message);
+                printf("Decoded message: %s\n", message);
                 break;
-
             case 3:
-                printf("\nExiting program...\n");
-                return 0;
-
+                printf("Exiting program.\n");
+                break;
             default:
-                printf("Invalid choice! Please try again.\n");
+                printf("Invalid choice. Please enter 1, 2, or 3.\n");
+                break;
         }
-    }
+
+    } while (choice != 3);
+
+    return 0;
 }
 
-
-void toggleBits(char *ch) {
-    *ch = *ch ^ (1 << 1); 
-    *ch = *ch ^ (1 << 4); 
-}
-
-
-void encodeMessage(char message[]) {
-    int len = strlen(message);
-    char temp;
-
-    
-    for (int i = 0; i < len / 2; i++) {
-        temp = message[i];
-        message[i] = message[len - i - 1];
-        message[len - i - 1] = temp;
-    }
-
-    
-    for (int i = 0; i < len; i++) {
-        toggleBits(&message[i]);
-    }
-}
-
-
-void decodeMessage(char message[]) {
-    int len = strlen(message);
-    char temp;
-
-   
-    for (int i = 0; i < len; i++) {
-        toggleBits(&message[i]);
-    }
-
-    
-    for (int i = 0; i < len / 2; i++) {
-        temp = message[i];
-        message[i] = message[len - i - 1];
-        message[len - i - 1] = temp;
-    }
-}
